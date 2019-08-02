b858e9d6caed46f4f4f8885cf282ecb5f1574bd5
Mr Zhu 404 <50517734+banghyuu@users.noreply.github.com>
2019-08-02 11:00:08 +0000

    Add files via upload

---
+++ wa.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/bash
+#IndonesianHWT
+#@otak_udang08
+#by MrZhu
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°|>"
+echo  $green    "|       Tools Spesial WhatsApp       |"
+echo  $green    "|                                    |"
+echo  $green    "| Author        : Mr Zhu 404         |"
+echo  $green    "| Recode        : Andi_Dougenk       |"
+echo  $green    "| My Team       : IndonesianErorTeam |"
+echo  $green    "| Thanks To     : Bakoel Es          |"
+echo  $green    "| With Love     : Bu Lurah           |"
+echo  $blue "<|°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "======================================="
+echo $blue "|| $green 1. Sadap WhatsApp  $blue              ||"
+echo $blue "|| $green 2. Spam WhatsApp       $blue          ||"
+echo $blue "|| $green 3. Nambah Kontak      $blue           ||"
+echo $blue "|| $green 0. Keluar          	$blue      ||"
+echo $blue "======================================="
+echo $blue"╭──[Tinggal Pilih Aja]>"
+read -p"╰───────root@dougenk=>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo
+echo $green"╭──["$red"Masukan Nomor Kamu"$green"]>"
+read -p"╰───────root@dougenk=•>" nama;
+sleep 1
+echo
+echo $green"╭──["$red"Masukan Nomor Target"$green"]>"
+read -p"╰───────root@dougenk=•>" cewek;
+sleep 1
+echo $green "Sedang melakukan penyadapan"
+sleep 1
+echo $red "Proses..."
+sleep 1
+echo $cyan "10% •"
+sleep 2
+echo $red "20% • •"
+sleep 2
+echo $cyan "30% • • •"
+sleep 2
+echo $red "40% • • • •"
+sleep 2
+echo $cyan "50% • • • • •"
+sleep 2
+echo $red "60% • • • • • •"
+sleep 2
+echo $cyan "70% • • • • • • •"
+sleep 2
+echo $red "80% • • • • • • • •"
+sleep 2
+echo $cyan "90% • • • • • • • • •"
+sleep 2
+echo $red "100% • • • • • • • • • •"
+sleep 2
+echo $green "SUKSES PAK DOUGENK :v !"
+sleep 2
+echo $red "Data :"
+echo
+echo  $green "Nomor kamu $nama"
+echo $green "Nomor target $cewek"
+sleep 1
+echo $green "Silahkan tunggu 10-15 menit"
+echo $green "Untuk mendapatkan kode verifikasi ilegal"
+echo $green "MrZhu & MrGenkz In Here"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+git clone https://github.com/siputra12/prank
+echo
+cd prank
+php wa.php
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Kontak cewek/cowok]>"
+read -p"╰───────root@dougenk=>" nama;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "+6281455119351"
+sleep 1
+echo $green "+6285745151698"
+sleep 1
+echo $cyan "+6281250274632"
+sleep 1
+echo $purple "+6285704868189"
+sleep 1
+echo $blue "+6282133059248"
+sleep 1
+echo $red "+6285892735649"
+sleep 1
+echo $green "+6281358841162"
+sleep 1
+echo $cyan "+6281216305976"
+sleep 1
+echo $purple "+6281333398025"
+sleep 1
+echo $blue "Terakhir Kontak MrZhu"
+sleep 1
+echo $blue "-0859175727510-"
+fi
+
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Jangan lupakan nama saya Mr Zhu"
+echo  $blue "Admin dari Indonesian Hazyweb Team!"
+echo  $green "Sampai jumpa ! pak dougenk :v"
+exit
+fi
